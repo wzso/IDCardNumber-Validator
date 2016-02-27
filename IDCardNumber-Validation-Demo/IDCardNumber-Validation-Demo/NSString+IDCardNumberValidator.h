221487e26c0281f4c30676fc7e2b3e638e68f4cf
wenzhishen <wenzhishen@icloud.com>
2016-02-27 11:35:29 +0800

    Add NSString Category

---
+++ IDCardNumber-Validation-Demo/IDCardNumber-Validation-Demo/NSString+IDCardNumberValidator.h
@@ -0,0 +1,13 @@
+//
+//  NSString+IDCardNumberValidator.h
+//  IDCardNumber-Validation-Demo
+//
+//  Created by Vincent on 2/26/16.
+//  Copyright © 2016 Vincent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (IDCardNumberValidator)
+- (BOOL)validIDCardNumber;
+@end
